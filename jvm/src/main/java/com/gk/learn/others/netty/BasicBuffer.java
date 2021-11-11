e737b5ec40f4ad8698423ff48d0b85e2bf3f4269
XUANJINYU <2238247045@qq.com>
2021-11-12 03:05:56 +0800

    filechannel

--- jvm/src/main/java/com/gk/learn/others/netty/BasicBuffer.java
+++ jvm/src/main/java/com/gk/learn/others/netty/BasicBuffer.java
@@ -3,6 +3,7 @@ package com.gk.learn.others.netty;
 import java.nio.IntBuffer;
 
 public class BasicBuffer {
+
 	public static void main(String[] args) {
 		// 创建buffer
 		IntBuffer intBuffer = IntBuffer.allocate(5);
@@ -12,6 +13,8 @@ public class BasicBuffer {
 		// todo: 重点注意 读写切换
 		intBuffer.flip();
 		// 判断还有没有元素
+//		intBuffer.position(1); //正常 0 2 4 6 8  设置position后，从position位置开始读取 2 4 6 8
+//		intBuffer.position(1); intBuffer.limit(2);  // 从1开始读，不能超过2的位置 会输出2
 		while (intBuffer.hasRemaining()) {
 			System.out.println(intBuffer.get());
 		}
